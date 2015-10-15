b63a093840cdfa4ddb86f746422656d89b9a6019
longyinzaitian <husyin@189.cn>
2015-10-15 20:12:58 +0800

    view基本完成，细节需要调节。

--- app/src/main/java/com/husy/rectanimation/RectAnimationView.java
+++ app/src/main/java/com/husy/rectanimation/RectAnimationView.java
@@ -7,14 +7,8 @@ import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Shader;
-import android.print.PrintAttributes;
 import android.util.AttributeSet;
-import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-
-import java.util.Random;
 
 /**
  * Created by husy on 2015/10/15.
@@ -25,10 +19,11 @@ import java.util.Random;
 public class RectAnimationView extends View {
     private Paint rectP;
     private Paint rectK;
-    private int rectW = 30;
-    private int count = 0;
-    private int screemW;
-    private int screemH;
+    private int rectW = 30;//每个矩形的宽度
+    private int count = 0;//矩形的个数
+    private int viewW;//组件的宽度
+    private int viewH;//组件的高度
+    private LinearGradient linearGradient;//线性渐变
     public RectAnimationView(Context context) {
         super(context);
         init();
@@ -44,28 +39,13 @@ public class RectAnimationView extends View {
         init();
     }
     private void init(){
-        rectP = new Paint();
-        rectP.setStrokeWidth(3);
+        rectP = new Paint();//初始化矩形的画笔
         rectP.setAntiAlias(true);
         rectP.setStyle(Paint.Style.FILL);
 
-        rectK = new Paint();
+        rectK = new Paint();//初始化边框的画笔
         rectK.setAntiAlias(true);
         rectK.setStyle(Paint.Style.STROKE);
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        LinearGradient linearGradient = new LinearGradient(
-                0,
-                0,
-                rectW,
-                randomH(),
-                new int[]{Color.BLUE,Color.YELLOW,Color.CYAN,Color.GREEN},
-                null,
-                Shader.TileMode.CLAMP);
-        rectP.setShader(linearGradient);
 
     }
 
@@ -81,32 +61,44 @@ public class RectAnimationView extends View {
         }
         if (modeH==MeasureSpec.UNSPECIFIED){
             height = 250;
-        }
+        }//当宽高不指定的时候，指定默认值
         setMeasuredDimension(width,height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        screemH = getHeight()-getPaddingLeft()-getPaddingRight();
-        screemW = getWidth()-getPaddingTop()-getPaddingBottom();
-        int top = screemH*2/3;
-        count = screemW/(rectW+20);
-        Rect rectw = new Rect(0,0,screemW,top);
+        //计算组件绘制区域的宽高
+        viewH = getHeight()-getPaddingLeft()-getPaddingRight();
+        viewW = getWidth()-getPaddingTop()-getPaddingBottom();
+        int bottom = viewH;//计算view底部距离
+        count = viewW /(rectW+10);//计算矩形的个数 10位矩形的间距
+        Rect rectw = new Rect(0,0, viewW,bottom);//外边框矩形
         canvas.drawRect(rectw, rectK);
+        //循环绘制矩形
         for (int i=0;i<count;i++){
+            linearGradient = new LinearGradient(
+                    0,
+                    0,
+                    rectW,
+                    randomH(),
+                    new int[]{Color.BLUE,Color.YELLOW,Color.CYAN,Color.GREEN},
+                    new float[]{0.3f,0.5f,0.7f,0.9f},
+                    Shader.TileMode.CLAMP);
+            rectP.setShader(linearGradient);
             Rect rect = new Rect(
-                    (int)(screemW*0.03f+i*(rectW+20)),
-                    top-randomH(),
-                    (int)(screemW*0.03f+(i)*(rectW+20)+rectW),
-                    top);
+                    (int)(viewW *0.03f+i*(rectW+10)),
+                    bottom-randomH(),
+                    (int)(viewW *0.03f+(i)*(rectW+10)+rectW),
+                    bottom);
             canvas.drawRect(rect,rectP);
         }
         canvas.save();
         postInvalidateDelayed(300);
     }
+    //随机生成矩形的高度，形成梯度
     private int randomH(){
         double rand = Math.random();
-        return (int)(rand*screemH*2/3);
+        return (int)(rand* viewH);
     }
 }
